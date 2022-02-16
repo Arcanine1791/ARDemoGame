4eb5cc754680b26cb4998a0eb5d40c8aedd0e0a6
Arcanine1791 <unityspaceboy@gmail.com>
2022-02-16 22:03:53 +0530

    1st pass

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent nvAgent;
+
+    void Start()
+    {
+        nvAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                nvAgent.destination = hit.point;
+            }
+        }
+
+        scoreText.text = "Coins:  "+score.ToString();
+
+        if(activequestion)
+        {
+            question.SetActive(true);
+            
+            activequestion = false;
+        }
+
+        if(score==50)
+        {
+            activequestion = true;
+        }
+    }
+
+
+    public Text scoreText,gamewin;
+    public int score;
+    public bool activequestion;
+
+    public GameObject girl, question;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="coins")
+        {
+            score += 10;
+           
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "question")
+        {
+            girl.SetActive(true);
+
+        }
+
+        if (other.gameObject.tag == "girl")
+        {
+            gamewin.text = " You have rescued the Girl";
+
+        }
+    }
+}
